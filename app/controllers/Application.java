98b8202828284779232c1f05c7d51fda9a1d2d38
Deepak Nayal <dnayal@gmail.com>
2012-11-14 19:23:16 +0000

    - First draft written for Experiences stream page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,44 @@
 package controllers;
 
+import handlers.ExperienceHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import play.Configuration;
+import play.Play;
 import play.mvc.Controller;
 import play.mvc.Result;
 import models.Experience;
 
+import utils.Util;
 import views.html.*;
 
 public class Application extends Controller {
+	
+	static Configuration config = Play.application().configuration();
   
 	public static Result index() {
 		return ok(index.render(Experience.find.all()));
 	}
 
-	// TODO - prepare a test case for all handlers
-	
+
+	public static Result getExperiences(String cityId, String categoryId) {
+		List<Experience> experienceList = new ArrayList<Experience>();
+		
+		if(Util.getString(categoryId).equalsIgnoreCase(config.getString("category.default")))
+			experienceList = ExperienceHandler.getExperiences(cityId);
+		else
+			experienceList = ExperienceHandler.getExperiences(cityId, categoryId);
+		
+		return ok(experiences.render(cityId, experienceList));
+		
+	}
+
+
+	public static Result getExperience(String experienceId) {
+		Experience experience = ExperienceHandler.getExperience(experienceId);
+		return TODO;
+	}
+
 }
